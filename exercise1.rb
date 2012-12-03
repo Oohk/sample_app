3cb71956c75576396bb72fd0876967bf858d0f12
Cal <dankhoolives@yahoo.com>
2012-12-02 22:00:28 -0800

    Finish layout and routes

---
+++ exercise1.rb
@@ -0,0 +1,8 @@
+PERSON1 = { first: "Michael", last: "JOnes" }
+PERSON2 = { first: "Missy", last: "Elliot" }
+PERSON3 = { first: "Whoam", last: "Orphi" }
+
+PARAMS = {}
+PARAMS[:father] = { first: "Michael", last: "JOnes" }
+PARAMS[:mother] = { first: "Missy", last: "Elliot" }
+PARAMS[:child] = { first: "Whoam", last: "Orphi" }
